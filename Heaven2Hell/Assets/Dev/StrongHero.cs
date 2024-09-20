c9ac17cec40ecd5500ea748164b23662b2e13bd8
teshawnwouter <145546485+teshawnwouter@users.noreply.github.com>
2024-09-20 21:22:03 +0200

    MetPseudo code voor tank hero

---
+++ Heaven2Hell/Assets/Dev/StrongHero.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrongHero : MonoBehaviour
+{ 
+  //Gemaakt door Teshawn,Elger,Akash,Mark Biesheuvel
+
+
+  //maak een vector3 en zet deze op de posities vande hero
+  //maak een int voor je kracht niveau en geef deze een waarde boven 0
+  //maak een int voor de kracht die der bij komt
+  //maak een game object en noem deze de hero
+
+  //we maken een class aan die noemen we heros
+  //maak een list waar je characters/heros kan opslaan
+
+  //maak een collision functie aan deze gaan colision als een colider hebben als para meter
+
+
+  //in de functie maak je een overlaps sphere deze plaatsen we op de hero(Vector3)
+
+  //in de overlaps sphere kijken we voor de tag spelers(deze regel je in de engine)
+  //kijk of spelers met de tag spelers niet null is
+  //voor elke speler met de tag speler tel je de kracht op met de extra kracht
+  //als de tag van spelers null is dan trek je de kracht die der bij komt af
+}
